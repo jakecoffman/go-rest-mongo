6a2d5a42f163db8d715d8e2bf391f249af32c03a
jake <jake@jakecoffman.com>
2016-10-21 21:06:54 -0500

    init user resource

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,12 +4,32 @@ import (
 	"github.com/gin-gonic/gin"
 	"gopkg.in/mgo.v2/bson"
 	"log"
+	"gopkg.in/mgo.v2"
+	"github.com/jakecoffman/go-rest-mongo/controllers"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	db := session.DB("test")
+
+	// For bootstrapping
+	db.DropDatabase()
+
 	r := gin.Default()
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, bson.M{"hello": "world!"})
 	})
+	userGroup := r.Group("/users")
+	{
+		userResource := controllers.NewUserResource(db)
+		userGroup.GET("/", userResource.List)
+		userGroup.GET("/:id", userResource.Get)
+	}
 	log.Fatal(r.Run("0.0.0.0:9898"))
 }
